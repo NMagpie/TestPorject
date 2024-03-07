811ab1c4ad14a429f2a05cd7c4d106a705e2d588
NMagpie <sorochin.work@gmail.com>
2024-03-07 15:17:06 +0200

    124

--- Program.cs
+++ Program.cs
@@ -1,9 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-
-Console.WriteLine("New line!");
-
-Console.WriteLine("Changes for a file conflict");
-
-Console.WriteLine("New feature line");
