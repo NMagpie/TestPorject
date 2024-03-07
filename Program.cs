a9322660f2a589bbad81add07980009880961aff
NMagpie <sorochin.work@gmail.com>
2024-03-07 11:54:57 +0200

    Program file was deleted

--- Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-Console.WriteLine("New line!");
-
-Console.WriteLine("Another line from another clone");
